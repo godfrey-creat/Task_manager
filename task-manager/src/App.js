a0b7a62212276d6616237556741b05ebe2943e04
godfrey-creat <ogodfrey135@gmail.com>
2024-06-14 14:29:17 +0300

    creating frontend index

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import TaskBoard from './components/TaskBoard';
+import DepartmentManagement from './components/DepartmentManagement';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/task-board" component={TaskBoard} />
+        <Route path="/department-management" component={DepartmentManagement} />
+        <Route path="/" component={Login} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
+
